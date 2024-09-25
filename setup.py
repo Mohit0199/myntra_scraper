3ab8f23dcec8e3dfcef03496cb14b71664e9a320
Mohit0199 <mohitrathod723@gmail.com>
2024-09-25 11:41:18 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="scraper",
+    version='0.0.1',
+    author='Mohit Rathod',
+    author_email="mohitrathod723@gmail.com",
+    packages=find_packages(),
+    install_requires = []
+)
